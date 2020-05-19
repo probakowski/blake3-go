680259668f2918ada34f4f3914632f1360ae692f
Przemko <przemko@przemko-robakowski.pl>
2020-05-19 17:59:32 +0200

    back to flat

--- blake3_test.go
+++ blake3_test.go
@@ -1,19 +1,18 @@
-package blake3_test
+package blake3
 
 import (
-	"blake3"
 	"encoding/hex"
 	"fmt"
 	"testing"
 )
 
 func TestHasher(t *testing.T) {
-	hasher := blake3.NewHasher()
+	hasher := NewHasher()
 	hasher.Write(make([]byte, 0))
 	bytes := hasher.Finalize(64)
 	fmt.Println(hex.EncodeToString(bytes))
 }
 
 func TestHash(t *testing.T) {
-	fmt.Println(hex.EncodeToString(blake3.Hash(make([]byte, 0), 64)))
+	fmt.Println(hex.EncodeToString(Hash(make([]byte, 0), 64)))
 }
